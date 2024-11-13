315a075d1c223dd1d5556112e72e62dae9f527ab
Kevin <kevinosborn31@gmail.com>
2024-11-13 23:00:58 +1100

    Better solution

--- arrays/number-of-fair-pairs.js
+++ arrays/number-of-fair-pairs.js
@@ -1,12 +1,34 @@
 const countFairPairs = (nums, lower, upper) => {
+    nums.sort((a, b) => a - b);  
     let currentPairs = 0;
+    
+    for (let i = 0; i < nums.length - 1; i++) {
+        let left = i + 1;
+        let right = nums.length - 1;
 
-    for (let p1 = 0; p1 < nums.length - 1; p1++) {
-        for (let p2 = p1 + 1; p2 < nums.length; p2++) {
-            const sum = nums[p1] + nums[p2];
-            if (sum >= lower && sum <= upper) {
-                currentPairs++;
+        while (left <= right) {
+            const mid = Math.floor((left + right) / 2);
+            if (nums[i] + nums[mid] >= lower) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
             }
+        }
+        const start = left;
+        left = i + 1;
+        right = nums.length - 1;
+        while (left <= right) {
+            const mid = Math.floor((left + right) / 2);
+            if (nums[i] + nums[mid] <= upper) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        const end = right;
+
+        if (start <= end) {
+            currentPairs += end - start + 1;
         }
     }
 
