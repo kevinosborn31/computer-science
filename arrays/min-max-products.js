7aed85c1880ca24a55b09a75e2b77d2ee45acd0a
Kevin <kevinosborn31@gmail.com>
2024-11-14 16:24:47 +1100

    Min max products solution

--- arrays/min-max-products.js
+++ arrays/min-max-products.js
@@ -1,3 +1,32 @@
 const minimizedMaximum = (n, quantities) => {
+        function canDistributeWithMaxProducts(maxProducts) {
+            let storesNeeded = 0;
+            
+            for (const quantity of quantities) {
+                storesNeeded += Math.ceil(quantity / maxProducts);
+                if (storesNeeded > n) {
+                    return false; 
+                }
+            }
+            
+            return storesNeeded <= n;
+        }
+    
+        let left = 1;
+        let right = Math.max(...quantities);
+        let result = right;
+    
+        while (left <= right) {
+            const mid = Math.floor((left + right) / 2);
+            
+            if (canDistributeWithMaxProducts(mid)) {
+                result = mid; 
+                right = mid - 1; 
+            } else {
+                left = mid + 1; 
+            }
+        }
+    
+        return result;
     
 };
