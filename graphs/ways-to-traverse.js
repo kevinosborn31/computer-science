7433051efe906c27f1021043904f8e0ea03960ef
Kevin <kevinosborn31@gmail.com>
2024-11-18 07:18:01 +1100

    Number of ways to traverse graph

---
+++ graphs/ways-to-traverse.js
@@ -0,0 +1,4 @@
+const numberOfWaysToTraverseGraph = (width, height) => {
+    if (width === 1 || height === 1) return 1;
+    return (numberOfWaysToTraverseGraph(width - 1, height)  + numberOfWaysToTraverseGraph(width, height - 1));
+  }
