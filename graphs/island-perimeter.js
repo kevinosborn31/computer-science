cebd136e610ef4f48e841ea0380f24cca8457176
Kevin <kevinosborn31@gmail.com>
2024-11-30 19:31:47 +1100

    BFS

---
+++ graphs/island-perimeter.js
@@ -0,0 +1,16 @@
+const islandPerimeter = (grid) => {
+  let runningTotal = 0;
+
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[i].length; j++) {
+      if (grid[i][j] === 1) {
+        if (i === 0 || grid[i - 1][j] === 0) runningTotal++;
+        if (i === grid.length - 1 || grid[i + 1][j] === 0) runningTotal++;
+        if (j === 0 || grid[i][j - 1] === 0) runningTotal++;
+        if (j === grid[i].length - 1 || grid[i][j + 1] === 0) runningTotal++;
+      }
+    }
+  }
+
+  return runningTotal;
+};
